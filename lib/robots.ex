585cd37545df4ef5d620d6475485e230d7caa5d5
Timothy Stott <stott.timothy@gmail.com>
2017-03-09 09:45:40 +0000

    Initial commit

---
+++ lib/robots.ex
@@ -0,0 +1,18 @@
+defmodule Robots do
+  @moduledoc """
+  Documentation for Robots.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Robots.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/robots_test.exs
@@ -0,0 +1,8 @@
+defmodule RobotsTest do
+  use ExUnit.Case
+  doctest Robots
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
