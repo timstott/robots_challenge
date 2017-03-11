6dabf5a188efddc83d0c05b0e7a8c3892394cd29
Timothy Stott <stott.timothy@gmail.com>
2017-03-11 15:46:53 +0000

    Move one robot in the world

--- lib/robots.ex
+++ lib/robots.ex
@@ -3,16 +3,36 @@ defmodule Robots do
   Documentation for Robots.
   """
 
-  @doc """
-  Hello world.
+  def run(world) do
+    world = Map.put(world, :results, [])
+    Enum.reduce(world.missions, world, fn(mission, world) ->
+      {world, robot} = Enum.reduce(mission.actions, {world, mission.robot}, fn(step, {world, robot}) ->
+        {world, move(robot, step)}
+      end)
+      %{world | results: (world.results ++ [robot])}
+    end).results
+  end
 
-  ## Examples
+  def move(robot, movement) when movement == :L or movement == :R do
+    bearings = [:W, :N, :E, :S]
+    index = Enum.find_index(bearings, fn(x) -> x == robot.bearing end)
+    offset = if (movement == :L), do: -1, else: 1
+    bearing = Enum.at(bearings, rem(index+offset+4, 4))
 
-      iex> Robots.hello
-      :world
+    %{robot | bearing: bearing}
+  end
 
-  """
-  def hello do
-    :world
+  def move(robot, :F) do
+    bearings_to_positions = %{
+      :W => {-1, 0},
+      :N => {0, 1},
+      :E => {1, 0},
+      :S => {0, -1}
+    }
+    {robot_x, robot_y} = robot.position
+    {move_x, move_y} = Map.get(bearings_to_positions, robot.bearing)
+    new_position = {robot_x + move_x, robot_y + move_y}
+
+    %{robot | position: new_position}
   end
 end
--- test/robots_test.exs
+++ test/robots_test.exs
@@ -2,7 +2,65 @@ defmodule RobotsTest do
   use ExUnit.Case
   doctest Robots
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "Robots.run/1" do
+    test "all robots termninate inside the world" do
+      world = %{
+        size: {5, 3},
+        missions: [
+          %{robot: %{position: {1, 1}, bearing: :E}, actions: [:R, :F, :R, :F, :R, :F, :R, :F]}
+        ]
+      }
+
+      output = [
+        %{position: {1, 1}, bearing: :E}
+      ]
+
+      assert Robots.run(world) == output
+    end
+  end
+
+  describe "Robots.move/2" do
+    test "robot moves bearing" do
+      robot = %{position: {0, 0}, bearing: :N}
+      assert Robots.move(robot, :L) == %{robot | bearing: :W}
+      assert Robots.move(robot, :R) == %{robot | bearing: :E}
+
+      robot = %{robot | bearing: :W}
+      assert Robots.move(robot, :L) == %{robot | bearing: :S}
+      assert Robots.move(robot, :R) == %{robot | bearing: :N}
+
+      robot = %{robot | bearing: :E}
+      assert Robots.move(robot, :L) == %{robot | bearing: :N}
+      assert Robots.move(robot, :R) == %{robot | bearing: :S}
+
+      robot = %{robot | bearing: :S}
+      assert Robots.move(robot, :L) == %{robot | bearing: :E}
+      assert Robots.move(robot, :R) == %{robot | bearing: :W}
+    end
+
+    test "robot moves position" do
+      robot = %{position: {5, 5}, bearing: :N}
+      assert Robots.move(robot, :F) == %{robot | position: {5, 6}}
+
+      robot = %{robot | bearing: :W}
+      assert Robots.move(robot, :F) == %{robot | position: {4, 5}}
+
+      robot = %{robot | bearing: :E}
+      assert Robots.move(robot, :F) == %{robot | position: {6, 5}}
+
+      robot = %{robot | bearing: :S}
+      assert Robots.move(robot, :F) == %{robot | position: {5, 4}}
+    end
+
+    test "robot moves bearing and position" do
+      robot = %{position: {0, 0}, bearing: :E}
+      new_robot = robot
+                  |> Robots.move(:F)
+                  |> Robots.move(:L)
+                  |> Robots.move(:F)
+                  |> Robots.move(:L)
+
+      assert new_robot == %{position: {1, 1}, bearing: :W}
+    end
   end
 end
