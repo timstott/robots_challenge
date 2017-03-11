c045373e64603566564a4503078c9dbbb79182b6
Timothy Stott <stott.timothy@gmail.com>
2017-03-11 19:57:24 +0000

    Move multiple robots in the world

--- lib/robots.ex
+++ lib/robots.ex
@@ -6,11 +6,44 @@ defmodule Robots do
   def run(world) do
     world = Map.put(world, :results, [])
     Enum.reduce(world.missions, world, fn(mission, world) ->
-      {world, robot} = Enum.reduce(mission.actions, {world, mission.robot}, fn(step, {world, robot}) ->
-        {world, move(robot, step)}
-      end)
+      robot = actionate(mission.actions, mission.robot, world)
       %{world | results: (world.results ++ [robot])}
     end).results
+  end
+
+  @doc """
+  Applies sequence of actions to a robot.
+  """
+  def actionate(actions, robot, world) do
+    Enum.reduce_while(actions, robot, fn(action, robot) ->
+      new_robot = move(robot, action)
+
+      new_robot_lost? = Robots.lost?(new_robot.position, world.size)
+      cond do
+        new_robot_lost? && Robots.any_lost?(robot, world.results) ->
+          {:cont, robot}
+        new_robot_lost? ->
+          {:halt, Map.put(robot, :lost, true)}
+        true ->
+          {:cont, new_robot}
+      end
+    end)
+  end
+
+  @doc """
+  Returns true when the robot position is outside the grid
+  """
+  def lost?({x_robot, y_robot}, {x_grid, y_grid}) do
+    x_robot > x_grid || y_robot > y_grid
+  end
+
+  @doc """
+  Retruns true when robots have been lost at the robot location
+  """
+  def any_lost?(%{position: robot_position}, robots) do
+    Enum.any?(robots, fn(%{position: was_position , lost: was_lost}) ->
+      was_lost && robot_position == was_position
+    end)
   end
 
   def move(robot, movement) when movement == :L or movement == :R do
--- test/robots_test.exs
+++ test/robots_test.exs
@@ -17,6 +17,99 @@ defmodule RobotsTest do
 
       assert Robots.run(world) == output
     end
+
+    test "one robot is lost" do
+      world = %{
+        size: {5, 3},
+        missions: [
+          %{robot: %{position: {3, 2}, bearing: :N}, actions: [:F, :R, :R, :F, :L, :L, :F, :F, :R, :R,:F, :L, :L]}
+        ]
+      }
+
+      output = [
+        %{position: {3, 3}, bearing: :N, lost: true}
+      ]
+
+      assert Robots.run(world) == output
+    end
+
+    test "one robot saved by lost robot" do
+      world = %{
+        size: {5, 3},
+        missions: [
+          %{robot: %{position: {3, 2}, bearing: :N}, actions: [:F, :R, :R, :F, :L, :L, :F, :F, :R, :R,:F, :L, :L]},
+          %{robot: %{position: {0, 3}, bearing: :W}, actions: [:L, :L, :F, :F, :F, :L, :F, :L, :F, :L]}
+        ]
+      }
+
+      output = [
+        %{position: {3, 3}, bearing: :N, lost: true},
+        %{position: {2, 3}, bearing: :S}
+      ]
+
+      assert Robots.run(world) == output
+    end
+  end
+
+  describe "Robots.actionate/2" do
+    test "robot moves inside the world" do
+      world = %{size: {1, 1}, results: []}
+
+      actions = [:F]
+      robot = %{position: {0, 0}, bearing: :N}
+
+      new_robot = %{robot | position: {0, 1}}
+      assert Robots.actionate(actions, robot, world) == new_robot
+    end
+
+    test "when the robot is lost actions terminate early" do
+      world = %{size: {1, 1}, results: []}
+
+      actions = [:F, :F, :R, :F]
+      robot = %{position: {0, 0}, bearing: :N}
+
+      new_robot = %{position: {0, 1}, bearing: :N, lost: true}
+      assert Robots.actionate(actions, robot, world) == new_robot
+    end
+
+    test "one lost robot and onve save by lost robot" do
+      lost_robot = %{position: {0, 1}, bearing: :N, lost: true}
+      world = %{size: {1, 1}, results: [lost_robot]}
+
+      actions = [:F, :F, :R, :R, :F]
+      robot = %{position: {0, 0}, bearing: :N}
+
+      new_robot = %{position: {0, 0}, bearing: :S}
+      assert Robots.actionate(actions, robot, world) == new_robot
+    end
+  end
+
+  describe "Robots.lost?/2" do
+    test "is true when position is outside the grid" do
+      position = {0, 2}
+      assert Robots.lost?(position, {1,1}) == true
+    end
+    test "is false when position is inside the grid" do
+      position = {0, 0}
+      assert Robots.lost?(position, {1,1}) == false
+    end
+  end
+
+  describe "Robots.any_lost?/2" do
+    test "is false when no robots were lost at this coordinate" do
+      robots = [
+        %{position: {0, 0}, lost: true}
+      ]
+      robot = %{position: {0, 5}}
+      assert Robots.any_lost?(robot, robots) == false
+    end
+    test "is true when one robot was lost at this coordinate" do
+      robots = [
+        %{position: {0, 5}, lost: true}
+      ]
+      robot = %{position: {0, 5}}
+      assert Robots.any_lost?(robot, robots) == true
+    end
   end
 
   describe "Robots.move/2" do
